239f71cbbdace5d5d9cf75779c995d04a9226165
yongwc <32865575+games130@users.noreply.github.com>
2019-07-03 17:21:18 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -5,7 +5,7 @@ const Version = "heplify-server 1.11"
 var Setting HeplifyServer
 
 type HeplifyServer struct {
-	BrokerAddr         string   `default:"127.0.0.1:4222"`
+	BrokerAddr         string   `default:"172.17.0.3:4222"`
 	BrokerTopic		   string   `default:"heplify.server.metric.1"`
 	BrokerQueue        string   `default:"hep.metric.queue.1"`
 	PromAddr           string   `default:":9096"`
