20aeb328660471c7718521c6b930f68d7d95b146
games130 <32865575+games130@users.noreply.github.com>
2019-07-04 15:50:08 +0800

    Add files via upload

--- metric/prometheus.go
+++ metric/prometheus.go
@@ -99,6 +99,7 @@ func (p *Prometheus) setup() (err error) {
 func (p *Prometheus) expose(hCh chan *decoder.HEP) {
 	for pkt := range hCh {
 		logp.Info("exposing some packet %s and %s", pkt.CID, pkt.FirstMethod)
+		fmt.Println("exposing some packet %s and %s", pkt.CID, pkt.FirstMethod)
 		packetsByType.WithLabelValues(pkt.NodeName, pkt.ProtoString).Inc()
 		packetsBySize.WithLabelValues(pkt.NodeName, pkt.ProtoString).Set(float64(len(pkt.Payload)))
 
